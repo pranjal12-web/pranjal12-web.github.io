ccf8d44fbc459a8c60f037b372e41c7026e3c102
pranjal12-web <67697534+pranjal12-web@users.noreply.github.com>
2020-08-24 18:35:15 +0530

    Add files via upload

--- calculator/index.js
+++ calculator/index.js
@@ -1,1 +1,49 @@
 console.log("connected");
+function add(){
+    var n1 = document.getElementById("one").value;
+    var n2 = document.getElementById("two").value;
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+    console.log(n1);
+    console.log(n2);
+    var result = n1+n2;
+    document.getElementById("res").innerHTML= result;
+}
+function sub(){
+    var n1 = document.getElementById("one").value;
+    var n2 = document.getElementById("two").value;
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+    var result = n1-n2
+    document.getElementById("res").innerHTML= result;
+}
+function mul(){
+    var n1 = document.getElementById("one").value;
+    var n2 = document.getElementById("two").value;
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+    var result = n1*n2
+    document.getElementById("res").innerHTML= result;
+
+}
+function div(){
+    var n1 = document.getElementById("one").value;
+var n2 = document.getElementById("two").value;
+n1 = parseInt(n1);
+n2 = parseInt(n2);
+var result = n1/n2
+document.getElementById("res").innerHTML= result;
+
+
+}
+function mod(){
+    var n1 = document.getElementById("one").value;
+    var n2 = document.getElementById("two").value;
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+    var result = n1%n2
+    document.getElementById("res").innerHTML= result;
+
+}
+
+
